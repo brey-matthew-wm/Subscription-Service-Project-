f422ca0a3ac24baed6b84695a0d4d00a1eca40ad
brey-matthew-wm <matthew.brey@west-mec.org>
2016-04-15 15:14:05 -0700

    final

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <title>CSS MenuMaker</title>
+</head>
+<body>
+
+<div id='cssmenu'>
+    <ul>
+
+        <li class='active'><a href='index.php'>Home</a></li>
+        <li><a href='gallery.html'>Gallery</a></li>
+        <li><a href='files/top_speeds.php'>Top Speeds</a></li>
+        <li><a href='about.html'>About Us</a></li>
+        <li><a href='signup.php'>Sign Up</a></li>
+    </ul>
+</div>
+
+<div id="main_page">
+  <div id="h1yo">  <h1>Welcome to MotoCentral!</h1> </div>
+            <img id="img1" src="images/Honda-7-Sport-Bike-Wallpaper.jpg" width="736" height="490">
+    <h3>Click on gallery for some more sweet pics, or click on the Top Speeds tab to view or add your own high speed!</h3>
+</div>
+
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <title>CSS MenuMaker</title>
+</head>
+<body>
+
+<div id='cssmenu'>
+    <ul>
+
+        <li><a href='index.php'>Home</a></li>
+        <li><a href='gallery.html'>Gallery</a></li>
+        <li><a href='files/top_speeds.php'>Top Speeds</a></li>
+        <li><a href='about.html'>About Us</a></li>
+        <li class="active"><a href='signup.php'>Sign Up</a></li>
+    </ul>
+</div>
+
+
+
+
+</body>
+</html>
